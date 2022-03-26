3b5b8817b3ebd97c5d8ffd77ebd59e76b1341764
hayashirafael <rafaelhayashi8@gmail.com>
2022-03-26 19:39:05 -0300

    feat: adicionando widget para a lista de todo

--- lib/pages/todo_list_page.dart
+++ lib/pages/todo_list_page.dart
@@ -1,38 +1,104 @@
 import 'package:flutter/material.dart';
+import 'package:todo_list_udemy/widgets/todo_list_item.dart';
 
-class TodoListPage extends StatelessWidget {
+class TodoListPage extends StatefulWidget {
   const TodoListPage({Key? key}) : super(key: key);
+
+  @override
+  State<TodoListPage> createState() => _TodoListPageState();
+}
+
+class _TodoListPageState extends State<TodoListPage> {
+  final TextEditingController todoController = TextEditingController();
+
+  List<String> todos = [];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 10),
-          child: Row(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
             children: [
-              const Expanded(
-                child: TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Adicionar tarefa',
-                    hintText: 'Fazer compras',
+              Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      controller: todoController,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Adicionar tarefa',
+                        hintText: 'Fazer compras',
+                      ),
+                    ),
                   ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 5),
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: const Color(0xff58d8b5),
+                        padding: const EdgeInsets.all(14),
+                      ),
+                      onPressed: () {
+                        String text = todoController.text;
+                        setState(() {
+                          todos.add(text);
+                        });
+                        todoController.clear();
+                      },
+                      child: const Icon(
+                        Icons.add,
+                        size: 30,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+              const SizedBox(height: 16),
+              Flexible(
+                child: ListView(
+                  shrinkWrap: true,
+                  children: [
+                    for (String todo in todos) const TodoListItem(),
+                    // ListTile(
+                    //   // selected: true,
+                    //   // dense: true,
+                    //   title: const Text('Tarefa 2'),
+                    //   subtitle: const Text('26/03/2022'),
+                    //   leading: const Icon(
+                    //     Icons.add_alarm,
+                    //     size: 30,
+                    //     color: Colors.blue,
+                    //     semanticLabel: 'Save',
+                    //   ),
+                    //   onTap: () {
+                    //     print('Alarme');
+                    //   },
+                    // ),
+                  ],
                 ),
               ),
-              Padding(
-                padding: const EdgeInsets.only(left: 5),
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    primary: const Color(0xff58d8b5),
-                    padding: const EdgeInsets.all(14),
+              const SizedBox(height: 16),
+              Row(
+                children: [
+                  const Expanded(
+                    child: Text('Você possui 0 tarefas pendentes'),
                   ),
-                  onPressed: () {},
-                  child: const Icon(
-                    Icons.add,
-                    size: 30,
-                  ),
-                ),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      primary: const Color(0xff58d8b5),
+                      padding: const EdgeInsets.all(14),
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        todos.clear();
+                      });
+                    },
+                    child: const Text('Limpar tudo'),
+                  )
+                ],
               )
             ],
           ),
---
+++ lib/widgets/todo_list_item.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class TodoListItem extends StatelessWidget {
+  const TodoListItem({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(5),
+        color: const Color.fromARGB(255, 117, 115, 115),
+      ),
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: const [
+          Text(
+            '20/11/2015',
+            style: TextStyle(fontSize: 12),
+          ),
+          Text(
+            'Tarefa 123',
+            style: TextStyle(fontWeight: FontWeight.w600, fontSize: 16),
+          ),
+        ],
+      ),
+    );
+  }
+}
