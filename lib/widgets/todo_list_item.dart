885d476772bc8b35d1da5e6f43edc038277f066c
hayashirafael <rafaelhayashi8@gmail.com>
2022-03-30 00:31:22 -0300

    fix: adicionando widget slidable

--- lib/widgets/todo_list_item.dart
+++ lib/widgets/todo_list_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:intl/intl.dart';
 import 'package:todo_list_udemy/models/todo.dart';
 
@@ -8,25 +9,47 @@ class TodoListItem extends StatelessWidget {
   final Todo todo;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.all(3),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(5),
-        color: const Color.fromARGB(255, 198, 198, 198),
-      ),
-      padding: const EdgeInsets.all(16),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            DateFormat('dd/MM/yyyy HH:mm').format(todo.dateTime),
-            style: const TextStyle(fontSize: 12),
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 3),
+      child: Slidable(
+        endActionPane: const ActionPane(
+          // dismissible: DismissiblePane(onDismissed: () {}),
+          children: [
+            // A SlidableAction can have an icon and/or a label.
+            SlidableAction(
+              onPressed: null,
+              backgroundColor: Color(0xFFFE4A49),
+              foregroundColor: Colors.white,
+              icon: Icons.delete,
+              label: 'Delete',
+              autoClose: true,
+            ),
+          ],
+          motion: ScrollMotion(),
+        ),
+        child: Container(
+          // width: MediaQuery.of(context).size.width,
+          // margin: const EdgeInsets.all(3),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(5),
+            color: const Color.fromARGB(255, 198, 198, 198),
           ),
-          Text(
-            todo.title,
-            style: const TextStyle(fontWeight: FontWeight.w600, fontSize: 16),
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text(
+                DateFormat('dd/MM/yyyy HH:mm').format(todo.dateTime),
+                style: const TextStyle(fontSize: 12),
+              ),
+              Text(
+                todo.title,
+                style:
+                    const TextStyle(fontWeight: FontWeight.w600, fontSize: 16),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
